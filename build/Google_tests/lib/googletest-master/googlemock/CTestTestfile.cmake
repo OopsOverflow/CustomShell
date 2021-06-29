cf9ad12ae3ca4c8837a08d8d29acdeb46b99a372
OopsOverflow <houssemmahmoudswe@gmail.com>
2021-06-29 16:18:37 +0200

    tests

--- build/Google_tests/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/esteban/CLionProjects/CustomShell/Google_tests/lib/googletest-master/googlemock
-# Build directory: /home/esteban/CLionProjects/CustomShell/build/Google_tests/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
---
+++ test/ShellTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by esteban on 29/06/2021.
+//
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(ShellTests, DemonstrateGTestMacros){
+    EXPECT_TRUE(false);
+}
